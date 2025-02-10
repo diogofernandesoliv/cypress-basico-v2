f5f1f887533274d1944b1d48b7c568c5491b2cac
Diogo Fernandes de Oliveira <diogofernandesoliv@gmail.com>
2025-02-10 16:42:30 -0300

    Subindo exercício 02

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,8 +1,22 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-  it('verifica o título da aplicação', function() {
+
+  beforeEach(() => {
     cy.visit('./src/index.html');
+  })
+
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+  })
+
+  it.only('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Diogo');
+    cy.get('#lastName').type('Fernandes de Oliveira');
+    cy.get('#email').type('diogofernandesq@gmail.com');
+    cy.get('#open-text-area').type('Quero aprender tudo sobre Cypress');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.success').should('be.visible');
   })
 })
